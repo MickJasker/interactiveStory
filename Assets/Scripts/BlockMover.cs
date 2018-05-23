642bee46fca55b656fe79f1b48645e42abaae387
Unknown <svenjanssen12321@gmail.com>
2018-05-23 13:05:19 +0200

    /Dialogue_Prequel: Auto stash before merge of "feature/Dialogue_Prequel" and "origin/feature/Dialogue_Prequel"

--- Assets/Scripts/BlockMover.cs
+++ Assets/Scripts/BlockMover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BlockMover : Interacter {
 
     List<Vector3> DeltaSpeed;
+    Vector3 DeltaPosition;
     public Camera OrthoCam;
 
     protected override void Start()
@@ -28,23 +29,63 @@ public class BlockMover : Interacter {
                 enabled = false;
             }
 
-            Vector3 pos = OrthoCam.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = 0;
-            transform.position = pos;
+            Vector3 pos = LongestAxis();
 
+            
+            if (Collision(pos))
+            {
+                //transform.position = pos;
+            }
+            Vector3 pos2 = OrthoCam.ScreenToWorldPoint(Input.mousePosition);
+            pos2.z = 0;
+            transform.position = pos2;
             yield return null;
         }
+    }
 
+    //determines axis furthes from the deltaposition, and returns the next position
+    Vector3 LongestAxis()
+    {
+        Vector3 pos = OrthoCam.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+
+        Vector3 distpos = pos - DeltaPosition;
+
+        UpdateDelta();
+
+        if (Mathf.Abs(distpos.x) >= Mathf.Abs(distpos.y))
+        {
+            return new Vector3(pos.x, 0, 0);
+        }
+        else
+        {
+            return new Vector3(0, pos.y, 0);
+        }
+    }
+
+    //Checks if there's a collider in front of the object
+    bool Collision(Vector3 direction)
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, direction, Color.yellow, Time.deltaTime);
+        if (Physics.Raycast(transform.position, direction, out hit))
+        {
+            return false;
+        }
+        
+        return true;
     }
 
     //keeps track of a list of positions of the last 10 frames
-    void UpdateDeltaSpeed()
+    void UpdateDelta()
     {
         DeltaSpeed.Add(transform.position);
         if (DeltaSpeed.Count > 10)
         {
             DeltaSpeed.RemoveAt(0);
         }
+
+        DeltaPosition = transform.position;
     }
 
     //Returns a Vector3 with the average speed over the last 10 frames
