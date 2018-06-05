9c5d33134221a6ef02611bc5ec374cb693857a3d
Unknown <svenjanssen12321@gmail.com>
2018-06-05 14:15:10 +0200

    Added click and time logger

---
+++ Assets/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logger : MonoBehaviour {
+
+    bool Active;
+    float Clicks;
+    int TimeTaken;
+
+    public Text TimeText;
+    public Text ClickText;
+
+	// Use this for initialization
+	void Start () {
+        Active = true;
+        StartCoroutine(Count());
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Movement>())
+        {
+            Active = false;
+        }
+    }
+
+    IEnumerator Count()
+    {
+        while (Active)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Clicks++;
+                ClickText.text = "Aantal Clicks: " + Clicks;
+            }
+
+            TimeTaken = (int)Time.time;
+            TimeText.text = "Tijd: " + TimeTaken + " seconden";
+
+            yield return null;
+        }
+    }
+}
